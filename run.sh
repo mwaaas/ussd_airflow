84c009d6e0980e47c86ada97854188aa156e8ee4
Francis Mwangi <francismwangi152@gmail.com>
2016-12-20 01:16:34 +0300

    fixed bug app server using the old naming (#25)

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --noinput
 # debugging with default server uncomment this and comment the gunicorn one
 #python manage.py runserver 0.0.0.0:3000 --settings=$DJANGO_SETTINGS_MODULE
 
-exec  gunicorn --bind=0.0.0.0:80 django_ussd_airflow.wsgi \
+exec  gunicorn --bind=0.0.0.0:80 ussd_airflow.wsgi \
         --workers=5\
         --log-level=info \
         --log-file=-\
--- worker_server.sh
+++ worker_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-celery -A django_ussd_airflow worker -E -n ussd-worker \
+celery -A ussd_airflow worker -E -n ussd-worker \
 --concurrency=3 \
 --loglevel=info
