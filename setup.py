8961ef01dc20e9de3114a5fc3a4ac9b82a6d96a0
Francis Mwangi <francismwangi152@gmail.com>
2017-01-17 15:19:30 +0300

    added application to be installed when doing pip install (#41)
    * added application to be installed when doing pip install
    
    * bumbed up the version

--- setup.py
+++ setup.py
@@ -1,12 +1,44 @@
 from setuptools import setup, find_packages
+import os
 
-__version__ = '0.0.1.7'
+__version__ = '0.0.1.8'
+
+
+def _strip_comments(l):
+    return l.split('#', 1)[0].strip()
+
+
+def _pip_requirement(req):
+    if req.startswith('-r '):
+        _, path = req.split()
+        return reqs(*path.split('/'))
+    return [req]
+
+
+def _reqs(*f):
+    return [
+        _pip_requirement(r) for r in (
+            _strip_comments(l) for l in open(
+                os.path.join(os.getcwd(), 'requirements', *f)).readlines()
+        ) if r]
+
+
+def reqs(*f):
+    """Parse requirement file.
+    Example:
+        reqs('default.txt')          # requirements/default.txt
+        reqs('extras', 'redis.txt')  # requirements/extras/redis.txt
+    Returns:
+        List[str]: list of requirements specified in the file.
+    """
+    return [req for subreq in _reqs(*f) for req in subreq]
 
 setup(
     name='ussd_airflow',
     version=__version__,
     packages=find_packages(exclude=('ussd_airflow',)),
     url='https://github.com/mwaaas/ussd_airflow',
+    install_requires=reqs('default.txt'),
     license='MIT',
     author='Mwas',
     author_email='mwasgize@gmail.com',
